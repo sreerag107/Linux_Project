f33e75816d3d587c4ad5a5a118896b90c08fb382
sreerag107 <84172177+sreerag107@users.noreply.github.com>
2026-07-07 19:40:33 +0530

    Add files via upload

---
+++ linux/lm75_pr/driver/lm75_driver.mod
@@ -0,0 +1,2 @@
+/home/sreerag/projects/lm75_pr/driver/lm75_driver.o
+
---
+++ linux/lm75_pr/driver/lm75_driver.mod.c
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x3b12617d, "module_layout" },
+	{ 0xaf5d72f5, "i2c_del_driver" },
+	{ 0xd3b80a4a, "i2c_register_driver" },
+	{ 0x86332725, "__stack_chk_fail" },
+	{ 0x482bc9de, "i2c_transfer_buffer_flags" },
+	{ 0x916ee6ac, "_dev_info" },
+	{ 0x8f678b07, "__stack_chk_guard" },
+	{ 0xefd6cf06, "__aeabi_unwind_cpp_pr0" },
+	{ 0xc5850110, "printk" },
+};
+
+MODULE_INFO(depends, "");
+
